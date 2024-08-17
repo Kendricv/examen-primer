dc1cd1867755313bd5f08c60ed4ec03551bcff7c
Kendricv <84217066+Kendricv@users.noreply.github.com>
2024-08-17 12:13:54 -0600

    Add files via upload

---
+++ PP Serie 3/script.js
@@ -0,0 +1,9 @@
+function showSection(sectionId) {
+    // Ocultar todas las secciones
+    const sections = document.querySelectorAll('.menu-section');
+    sections.forEach(section => section.style.display = 'none');
+
+    // Mostrar la sección seleccionada
+    const sectionToShow = document.getElementById(sectionId);
+    sectionToShow.style.display = 'block';
+}
